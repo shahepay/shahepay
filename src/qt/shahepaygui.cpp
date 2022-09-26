b463ed14c1b51a5bd4c6262556bb2a4db0e464e4
shahepay <103252655+shahepay@users.noreply.github.com>
2022-09-27 00:58:23 +0700

    disabled asset

--- src/qt/shahepaygui.cpp
+++ src/qt/shahepaygui.cpp
@@ -629,12 +629,12 @@ void shahepayGUI::createToolBars()
         toolbar->addAction(sendCoinsAction);
         toolbar->addAction(receiveCoinsAction);
         toolbar->addAction(historyAction);
-        toolbar->addAction(createAssetAction);
-        toolbar->addAction(transferAssetAction);
-        toolbar->addAction(manageAssetAction);
+//        toolbar->addAction(createAssetAction);
+//        toolbar->addAction(transferAssetAction);
+//        toolbar->addAction(manageAssetAction);
 //        toolbar->addAction(messagingAction);
 //        toolbar->addAction(votingAction);
-        toolbar->addAction(restrictedAssetAction);
+//        toolbar->addAction(restrictedAssetAction);
 
         QString openSansFontString = "font: normal 22pt \"Open Sans\";";
         QString normalString = "font: normal 22pt \"Arial\";";
